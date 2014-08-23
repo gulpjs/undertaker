21a301cf6de044e7f11227490981d78183a13055
Blaine Bublitz <blaine.bublitz@gmail.com>
2014-08-22 18:21:30 -0700

    New: First implementation

---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var inherits = require('util').inherits;
+var EventEmitter = require('events').EventEmitter;
+
+var bach = require('bach');
+var DefaultRegistry = require('undertaker-registry');
+
+var metadata = require('./lib/metadata');
+var normalizeArgs = require('./lib/normalizeArgs');
+var createExtensions = require('./lib/createExtensions');
+
+function Undertaker(Registry){
+  EventEmitter.call(this);
+
+  Registry = Registry || DefaultRegistry;
+
+  this.registry = new Registry();
+}
+
+inherits(Undertaker, EventEmitter);
+
+Undertaker.prototype.get = function get(name){
+  var wrapper = this.registry.get(name);
+
+  if(!wrapper){
+    return;
+  }
+
+  var meta = metadata.get(wrapper);
+
+  if(meta){
+    return meta.orig;
+  }
+
+  return wrapper;
+};
+
+Undertaker.prototype.set = function set(name, fn){
+  function taskWrapper(){
+    /* jshint validthis: true */
+    return fn.apply(this, arguments);
+  }
+
+  metadata.set(taskWrapper, {
+    name: name,
+    orig: fn
+  });
+
+  this.registry.set(name, taskWrapper);
+};
+
+Undertaker.prototype.task = function task(name, fn){
+  if(typeof name === 'function'){
+    fn = name;
+    name = fn.name;
+  }
+
+  if(!fn){
+    return this.get(name);
+  }
+
+  this.set(name, fn);
+};
+
+Undertaker.prototype.series = function series(){
+  var args = normalizeArgs(this.registry, arguments);
+  var extensions = createExtensions(this);
+  var fn = bach.series(args, extensions);
+  metadata.set(fn, { name: 'series' });
+  return fn;
+};
+
+Undertaker.prototype.parallel = function parallel(){
+  var args = normalizeArgs(this.registry, arguments);
+  var extensions = createExtensions(this);
+  var fn = bach.parallel(args, extensions);
+  metadata.set(fn, { name: 'parallel' });
+  return fn;
+};
+
+module.exports = Undertaker;
---
+++ test/series.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var lab = exports.lab = require('lab').script();
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var beforeEach = lab.beforeEach;
+var after = lab.after;
+var afterEach = lab.afterEach;
+var expect = require('lab').expect;
+
+var Undertaker = require('../');
+
+function fn1(done){
+  done(null, 1);
+}
+
+function fn2(done){
+  setTimeout(function(){
+    done(null, 2);
+  }, 500);
+}
+
+function fn3(done){
+  done(null, 3);
+}
+
+function fnError(done){
+  done(new Error('An Error Occurred'));
+}
+
+describe('series', function(){
+
+  var taker;
+
+  beforeEach(function(done){
+    taker = new Undertaker();
+    taker.task('test1', fn1);
+    taker.task('test2', fn2);
+    taker.task('test3', fn3);
+    taker.task('error', fnError);
+    done();
+  });
+
+  it('should take all string names', function(done){
+    taker.series('test1', 'test2', 'test3')(function(err, results){
+      expect(results).to.deep.equal([1, 2, 3]);
+      done(err);
+    });
+  });
+
+  it('should take all functions', function(done){
+    taker.series(fn1, fn2, fn3)(function(err, results){
+      expect(results).to.deep.equal([1, 2, 3]);
+      done(err);
+    });
+  });
+
+  it('should take string names and functions', function(done){
+    taker.series('test1', fn2, 'test3')(function(err, results){
+      expect(results).to.deep.equal([1, 2, 3]);
+      done(err);
+    });
+  });
+
+  it('should take nested series', function(done){
+    var series1 = taker.series('test1', 'test2', 'test3');
+    taker.series('test1', series1, 'test3')(function(err, results){
+      expect(results).to.deep.equal([1, [1, 2, 3], 3]);
+      done(err);
+    });
+  });
+
+  it('should stop processing on error', function(done){
+    taker.on('error', function(err){
+      // to keep the process from crashing
+    });
+    taker.series('test1', 'error', 'test3')(function(err, results){
+      expect(err).to.be.an.instanceof(Error);
+      expect(results).to.deep.equal([1, undefined, undefined]);
+      done();
+    });
+  });
+
+  it('should throw on unregistered task', function(done){
+    function unregistered(){
+      taker.series('unregistered');
+    }
+
+    expect(unregistered).to.throw('Task never defined: unregistered');
+    done();
+  });
+});
