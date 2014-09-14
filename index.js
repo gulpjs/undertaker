2be1710812050a96a4e41045b28a18e7c5322a70
Blaine Bublitz <blaine.bublitz@gmail.com>
2014-09-13 18:18:06 -0700

    Update: Reorganize the project structure

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 var inherits = require('util').inherits;
 var EventEmitter = require('events').EventEmitter;
 
-var _ = require('lodash');
-var bach = require('bach');
 var DefaultRegistry = require('undertaker-registry');
 
-var metadata = require('./lib/metadata');
-var buildTree = require('./lib/buildTree');
-var normalizeArgs = require('./lib/normalizeArgs');
-var createExtensions = require('./lib/createExtensions');
-var validateRegistry = require('./lib/validateRegistry');
+var get = require('./lib/get');
+var set = require('./lib/set');
+var tree = require('./lib/tree');
+var task = require('./lib/task');
+var series = require('./lib/series');
+var parallel = require('./lib/parallel');
+var validateRegistry = require('./lib/helpers/validateRegistry');
 
 function Undertaker(Registry){
   EventEmitter.call(this);
@@ -25,105 +25,16 @@ function Undertaker(Registry){
 
 inherits(Undertaker, EventEmitter);
 
-Undertaker.prototype.get = function get(name){
-  var wrapper = this.registry.get(name);
+Undertaker.prototype.get = get;
 
-  if(!wrapper){
-    return;
-  }
+Undertaker.prototype.set = set;
 
-  var meta = metadata.get(wrapper);
+Undertaker.prototype.tree = tree;
 
-  if(meta){
-    return meta.orig;
-  }
+Undertaker.prototype.task = task;
 
-  return wrapper;
-};
+Undertaker.prototype.series = series;
 
-Undertaker.prototype.set = function set(name, fn){
-  function taskWrapper(){
-    /* jshint validthis: true */
-    return fn.apply(this, arguments);
-  }
-
-  var meta = metadata.get(fn) || {};
-  var nodes = [];
-  if(meta.tree){
-    nodes.push(meta.tree);
-  }
-
-  metadata.set(taskWrapper, {
-    name: name,
-    orig: fn,
-    tree: {
-      label: name,
-      type: 'task',
-      nodes: nodes
-    }
-  });
-
-  this.registry.set(name, taskWrapper);
-};
-
-Undertaker.prototype.tree = function tree(opts){
-  opts = _.defaults(opts || {}, {
-    deep: false
-  });
-
-  var tasks = this.registry.tasks();
-  return _.map(tasks, function(task){
-    var meta = metadata.get(task);
-
-    if(opts.deep){
-      return meta.tree;
-    }
-
-    return meta.tree.label;
-  });
-};
-
-Undertaker.prototype.task = function task(name, fn){
-  if(typeof name === 'function'){
-    fn = name;
-    name = fn.name;
-  }
-
-  if(!fn){
-    return this.get(name);
-  }
-
-  this.set(name, fn);
-};
-
-Undertaker.prototype.series = function series(){
-  var args = normalizeArgs(this.registry, arguments);
-  var extensions = createExtensions(this);
-  var fn = bach.series(args, extensions);
-  metadata.set(fn, {
-    name: 'series',
-    tree: {
-      label: '<series>',
-      type: 'function',
-      nodes: buildTree(args)
-    }
-  });
-  return fn;
-};
-
-Undertaker.prototype.parallel = function parallel(){
-  var args = normalizeArgs(this.registry, arguments);
-  var extensions = createExtensions(this);
-  var fn = bach.parallel(args, extensions);
-  metadata.set(fn, {
-    name: 'parallel',
-    tree: {
-      label: '<parallel>',
-      type: 'function',
-      nodes: buildTree(args)
-    }
-  });
-  return fn;
-};
+Undertaker.prototype.parallel = parallel;
 
 module.exports = Undertaker;
