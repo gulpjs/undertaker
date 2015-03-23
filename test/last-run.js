dbae6c0ca78b582e247032a63a194bac8dd8cd67
Damien Lebrun <dinoboff@hotmail.com>
2015-03-23 00:06:00 +0000

    Update: Add task id to task event

--- test/last-run.js
+++ test/last-run.js
@@ -10,19 +10,24 @@ var afterEach = lab.afterEach;
 
 var Undertaker = require('../');
 var lastRunLib = require('../lib/last-run');
+var metadata = require('../lib/helpers/metadata');
 
-function fn(done){
-  done();
-}
 
-describe('lastRun', function(){
-  var taker, defaultResolution = process.env.UNDERTAKER_TIME_RESOLUTION;
+describe('lastRun', function() {
+  var taker, defaultResolution = process.env.UNDERTAKER_TIME_RESOLUTION, test1, test2, test1Id;
 
-  beforeEach(function(done){
+  beforeEach(function(done) {
     process.env.UNDERTAKER_TIME_RESOLUTION = '0';
     taker = new Undertaker();
-    taker.task('test1', fn);
-    taker.task('test2', fn);
+
+    test1 = function(cb) { cb(); };
+    taker.task('test1', test1);
+    test1Id = metadata.getTaskId(test1);
+
+    test2 = function(cb) { cb(); };
+    test2.displayName = 'test2';
+    taker.task(test2);
+
     done();
   });
 
@@ -62,26 +67,40 @@ describe('lastRun', function(){
       done();
     });
 
+    it('should set default resolution to UNDERTAKER_TIME_RESOLUTION', function(done) {
+      process.env.UNDERTAKER_TIME_RESOLUTION = '2000';
+      expect(lastRunLib.defaultResolution()).to.equal(2000);
+      done();
+    });
+
   });
 
-  it('should record tasks time execution', function(done){
+  it('should record tasks time execution', function(done) {
     var since = Date.now();
-    taker.parallel('test1')(function(err){
+    taker.parallel('test1')(function(err) {
       expect(taker.lastRun('test1')).to.exist();
       expect(taker.lastRun('test1')).to.be.within(since, Date.now());
-      expect(taker.lastRun('test2')).to.not.exist();
-      expect(taker.lastRun('notexists')).to.not.exist();
+      expect(taker.lastRun(test2)).to.not.exist();
+      expect(taker.lastRun(function(){})).to.not.exist();
+      expect(taker.lastRun.bind(taker, 'notexists')).to.throw(Error);
       done(err);
     });
   });
 
   it('should record all tasks time execution', function(done){
     var since = Date.now();
-    taker.parallel('test1', 'test2')(function(err){
+    taker.parallel('test1', test2)(function(err){
       expect(taker.lastRun('test1')).to.exist();
       expect(taker.lastRun('test1')).to.be.within(since, Date.now());
-      expect(taker.lastRun('test2')).to.exist();
-      expect(taker.lastRun('test2')).to.be.within(since, Date.now());
+      expect(taker.lastRun(test2)).to.exist();
+      expect(taker.lastRun(test2)).to.be.within(since, Date.now());
+      done(err);
+    });
+  });
+
+  it('should record same tasks time execution for a string task and its original', function(done){
+    taker.series(test2)(function(err){
+      expect(taker.lastRun(test2)).to.equal(taker.lastRun('test2'));
       done(err);
     });
   });
@@ -89,8 +108,8 @@ describe('lastRun', function(){
   it('should record task start time', function(done){
     var since = Date.now();
 
-    taker.emit('start', {uid: 1, name: 'test1', time: since});
-    taker.emit('stop', {uid: 1, name: 'test1', time: since + 1});
+    taker.emit('start', {uid: 1, taskId: test1Id, name: 'test1', time: since});
+    taker.emit('stop', {uid: 1, taskId: test1Id, name: 'test1', time: since + 1});
 
     expect(taker.lastRun('test1')).to.equal(since);
     done();
@@ -100,8 +119,9 @@ describe('lastRun', function(){
     var since = 1426000000111;
     var expected = 1426000000000;
 
-    taker.emit('start', {uid: 1, name: 'test1', time: since});
-    taker.emit('stop', {uid: 1, name: 'test1', time: since + 1});
+    taker._registry.get('test1')._uid = 0;
+    taker.emit('start', {uid: 1, taskId: test1Id, name: 'test1', time: since});
+    taker.emit('stop', {uid: 1, taskId: test1Id, name: 'test1', time: since + 1});
 
     expect(taker.lastRun('test1', 1000)).to.equal(expected);
     done();
@@ -110,8 +130,9 @@ describe('lastRun', function(){
   it('should not record task start time on error', function(done){
     var since = Date.now();
 
-    taker.emit('start', {uid: 1, name: 'test1', time: since});
-    taker.emit('error', {uid: 1, name: 'test1'});
+    taker._registry.get('test1')._uid = 0;
+    taker.emit('start', {uid: 1, taskId: test1Id, name: 'test1', time: since});
+    taker.emit('error', {uid: 1, taskId: test1Id, name: 'test1'});
 
     expect(taker.lastRun('test1')).to.not.exist();
     done();
@@ -120,10 +141,11 @@ describe('lastRun', function(){
   it('should keep last record task start time on error', function(done){
     var since = Date.now();
 
-    taker.emit('start', {uid: 1, name: 'test1', time: since});
-    taker.emit('stop', {uid: 1, name: 'test1', time: since + 1});
-    taker.emit('start', {uid: 1, name: 'test1', time: since + 2});
-    taker.emit('error', {uid: 1, name: 'test1'});
+    taker._registry.get('test1')._uid = 0;
+    taker.emit('start', {uid: 1, taskId: test1Id, name: 'test1', time: since});
+    taker.emit('stop', {uid: 1, taskId: test1Id, name: 'test1', time: since + 1});
+    taker.emit('start', {uid: 1, taskId: test1Id, name: 'test1', time: since + 2});
+    taker.emit('error', {uid: 1, taskId: test1Id, name: 'test1'});
 
     expect(taker.lastRun('test1')).to.equal(since);
     done();
@@ -132,10 +154,11 @@ describe('lastRun', function(){
   it('should record last task start time on concurrent run', function(done){
     var since = Date.now();
 
-    taker.emit('start', {uid: 1, name: 'test1', time: since - 1});
-    taker.emit('start', {uid: 2, name: 'test1', time: since});
-    taker.emit('stop', {uid: 2, name: 'test1', time: since + 1});
-    taker.emit('stop', {uid: 1, name: 'test1', time: since + 2});
+    taker._registry.get('test1')._uid = 0;
+    taker.emit('start', {uid: 1, taskId: test1Id, name: 'test1', time: since - 1});
+    taker.emit('start', {uid: 2, taskId: test1Id, name: 'test1', time: since});
+    taker.emit('stop', {uid: 2, taskId: test1Id, name: 'test1', time: since + 1});
+    taker.emit('stop', {uid: 1, taskId: test1Id, name: 'test1', time: since + 2});
 
     expect(taker.lastRun('test1')).to.equal(since);
     done();
@@ -144,10 +167,11 @@ describe('lastRun', function(){
   it('should record last task start time on concurrent run with error', function(done){
     var since = Date.now();
 
-    taker.emit('start', {uid: 1, name: 'test1', time: since - 1});
-    taker.emit('start', {uid: 2, name: 'test1', time: since});
-    taker.emit('stop', {uid: 2, name: 'test1', time: since + 1});
-    taker.emit('error', {uid: 1, name: 'test1'});
+    taker._registry.get('test1')._uid = 0;
+    taker.emit('start', {uid: 1, taskId: test1Id, name: 'test1', time: since - 1});
+    taker.emit('start', {uid: 2, taskId: test1Id, name: 'test1', time: since});
+    taker.emit('stop', {uid: 2, taskId: test1Id, name: 'test1', time: since + 1});
+    taker.emit('error', {uid: 1, taskId: test1Id, name: 'test1'});
 
     expect(taker.lastRun('test1')).to.equal(since);
     done();
@@ -156,10 +180,11 @@ describe('lastRun', function(){
   it('should record last task start time on concurrent run with error first', function(done){
     var since = Date.now();
 
-    taker.emit('start', {uid: 1, name: 'test1', time: since - 1});
-    taker.emit('start', {uid: 2, name: 'test1', time: since});
-    taker.emit('error', {uid: 1, name: 'test1'});
-    taker.emit('stop', {uid: 2, name: 'test1', time: since + 1});
+    taker._registry.get('test1')._uid = 0;
+    taker.emit('start', {uid: 1, taskId: test1Id, name: 'test1', time: since - 1});
+    taker.emit('start', {uid: 2, taskId: test1Id, name: 'test1', time: since});
+    taker.emit('error', {uid: 1, taskId: test1Id, name: 'test1'});
+    taker.emit('stop', {uid: 2, taskId: test1Id, name: 'test1', time: since + 1});
 
     expect(taker.lastRun('test1')).to.equal(since);
     done();
@@ -168,25 +193,27 @@ describe('lastRun', function(){
   it('keep last record task start time on unexpected error', function(done){
     var since = Date.now();
 
+    taker._registry.get('test1')._uid = 0;
+
     // stop event without any start event
-    taker.emit('stop', {uid: 1, name: 'test1', time: since - 4});
+    taker.emit('stop', {uid: 1, taskId: test1Id, name: 'test1', time: since - 4});
     expect(taker.lastRun('test1')).to.not.exist();
 
     // error event without any start event
-    taker.emit('error', {uid: 2, name: 'test1', time: since - 3});
+    taker.emit('error', {uid: 2, taskId: test1Id, name: 'test1', time: since - 3});
     expect(taker.lastRun('test1')).to.not.exist();
 
     // Start event without start time
-    taker.emit('start', {uid: 3, name: 'test1'});
-    taker.emit('stop', {uid: 3, name: 'test1', time: since - 1});
+    taker.emit('start', {uid: 3, taskId: test1Id, name: 'test1'});
+    taker.emit('stop', {uid: 3, taskId: test1Id, name: 'test1', time: since - 1});
     expect(taker.lastRun('test1')).to.not.exist();
 
-    taker.emit('start', {uid: 4, name: 'test1', time: since});
-    taker.emit('stop', {uid: 4, name: 'test1', time: since + 1});
+    taker.emit('start', {uid: 4, taskId: test1Id, name: 'test1', time: since});
+    taker.emit('stop', {uid: 4, taskId: test1Id, name: 'test1', time: since + 1});
     expect(taker.lastRun('test1')).to.equal(since);
 
     // stop event without matching start event
-    taker.emit('stop', {uid: 5, name: 'test1', time: since + 2});
+    taker.emit('stop', {uid: 5, taskId: test1Id, name: 'test1', time: since + 2});
     expect(taker.lastRun('test1')).to.equal(since);
 
     done();
