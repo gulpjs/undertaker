a366de83621fa7ea20087e87123c8d7c0a7b6285
erikkemperman <erikkemperman@gmail.com>
2015-10-20 15:49:20 +0200

    Add some tests for task aliases

---
+++ test/fixtures/taskTree/aliasNested.js
@@ -0,0 +1,98 @@
+'use strict';
+
+module.exports = [
+  {
+    label: 'noop',
+    type: 'task',
+    nodes: []
+  },
+  {
+    label: 'fn1',
+    type: 'task',
+    nodes: []
+  },
+  {
+    label: 'fn2',
+    type: 'task',
+    nodes: []
+  },
+  {
+    label: 'fn3',
+    type: 'task',
+    nodes: []
+  },
+  {
+    label: 'ser',
+    type: 'task',
+    nodes: [
+      {
+        label: '<series>',
+        type: 'function',
+        nodes: [
+          {
+            label: 'noop',
+            type: 'function',
+            nodes: []
+          },
+          {
+            label: '<anonymous>',
+            type: 'function',
+            nodes: []
+          },
+          {
+            label: 'fn1',
+            type: 'task',
+            nodes: []
+          },
+          {
+            label: 'fn2',
+            type: 'task',
+            nodes: []
+          },
+          {
+            label: 'fn3',
+            type: 'task',
+            nodes: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'par',
+    type: 'task',
+    nodes: [
+      {
+        label: '<parallel>',
+        type: 'function',
+        nodes: [
+          {
+            label: 'noop',
+            type: 'function',
+            nodes: []
+          },
+          {
+            label: '<anonymous>',
+            type: 'function',
+            nodes: []
+          },
+          {
+            label: 'fn1',
+            type: 'task',
+            nodes: []
+          },
+          {
+            label: 'fn2',
+            type: 'task',
+            nodes: []
+          },
+          {
+            label: 'fn3',
+            type: 'task',
+            nodes: []
+          }
+        ]
+      }
+    ]
+  }
+];
