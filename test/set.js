edd34876e52cd55ff2b286d0a2619bb8fd4a18d5
Blaine Bublitz <blaine.bublitz@gmail.com>
2016-06-22 16:00:08 -0700

    Fix: Support modified functions in custom registries

--- test/set.js
+++ test/set.js
@@ -12,6 +12,9 @@ var afterEach = lab.afterEach;
 
 var Undertaker = require('../');
 
+var MetadataRegistry = require('undertaker-task-metadata');
+var metadata = require('../lib/helpers/metadata');
+
 function noop(done){ done(); }
 
 describe('set', function(){
@@ -26,6 +29,15 @@ describe('set', function(){
   it('should register a function by string name', function(done){
     taker.set('test1', noop);
     expect(taker.get('test1')).to.equal(noop);
+    done();
+  });
+
+  it('should attach metadata based on the return value of _registry.set', function(done){
+    taker.registry(new MetadataRegistry());
+    taker.set('test1', noop);
+    var meta = metadata.get(taker._registry._tasks.test1);
+    expect(meta).to.exist();
+    expect(meta.name).to.equal('test1');
     done();
   });
 
